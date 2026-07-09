e9b65a0b0ffcb1c6131bfd24d2d21654f8f96783
Arthur <arthur@local>
2026-07-09 19:33:16 +0200

    refactor

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 import './style.css'
 import './app.js'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+
+  onNeedRefresh() {
+    location.reload()
+  }
+})
