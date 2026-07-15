7b02959a2dc39150e4b8fc3e129990698506aa71
Arthur <arthur@local>
2026-07-15 21:29:02 +0200

    bugfix

--- src/sw.js
+++ src/sw.js
@@ -26,7 +26,9 @@ precacheAndRoute(self.__WB_MANIFEST);
 //
 // SPA-Navigation immer aus dem Precache
 //
-createHandlerBoundToURL(import.meta.env.BASE_URL + "index.html");
+const navigationHandler = createHandlerBoundToURL(
+  import.meta.env.BASE_URL + "index.html"
+);
 
 registerRoute(new NavigationRoute(navigationHandler));
 
